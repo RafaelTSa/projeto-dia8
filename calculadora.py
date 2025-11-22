fd67e4d266cc41fc110e7b8951d42c1c756c0f40
Rafael T Santos <texera09@gmail.com>
2025-11-21 22:13:23 -0300

    adicona um print para mostrar ao usuario os números digitados

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,5 @@
 numero_1 = int(input("Digite seu primeiro número: "))
 numero_2 = int(input("Digite seu segundo número: "))
 
+print('{} + {} = '.format(numero_1, numero_2))
 print(numero_1 + numero_2)
