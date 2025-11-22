99bde78cc2fcd93bff633d64da61e71cfb90f487
Rafael T Santos <texera09@gmail.com>
2025-11-21 22:17:20 -0300

    adiciona operadores, soma, subtração, multiplicação, divisão

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,19 @@
 numero_1 = int(input("Digite seu primeiro número: "))
 numero_2 = int(input("Digite seu segundo número: "))
 
+# Soma
 print('{} + {} = '.format(numero_1, numero_2))
 print(numero_1 + numero_2)
+
+# Subtração
+print('{} - {} = '.format(numero_1, numero_2))
+print(numero_1 - numero_2)
+
+# Multiplicação
+print('{} * {} = '.format(numero_1, numero_2))
+print(numero_1 * numero_2)
+
+# Divisão
+print('{} / {} = '.format(numero_1, numero_2))
+print(numero_1 / numero_2)
+
