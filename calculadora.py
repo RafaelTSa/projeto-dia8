14251c6af170daa61db11cae10fca6a8acca77d7
Rafael T Santos <texera09@gmail.com>
2025-11-21 23:13:06 -0300

    corrige erro de identação

--- calculadora.py
+++ calculadora.py
@@ -1,38 +1,60 @@
 # Define nossa função
 def calculate():
-    operação = input('''
+    operacao = input('''
 Por favor, selecione a operação desejada:
 + para soma
 - para subtração
 * para multiplicação
 / para divisão
 ''')
-    numero_1 = int(input("Digite seu primeiro número: "))
-    numero_2 = int(input("Digite seu segundo número: "))
+    try:
+        numero_1 = int(input("Digite seu primeiro número: "))
+        numero_2 = int(input("Digite seu segundo número: "))
+    except ValueError:
+        print("Entrada inválida. Por favor, digite apenas números.")
+        again()
+        return
 
     # Soma
-    if operação == '+':
+    if operacao == '+':
         print('{} + {} = '.format(numero_1, numero_2))
         print(numero_1 + numero_2)
 
     # Subtração
-    elif operação == '-':
+    elif operacao == '-':
         print('{} - {} = '.format(numero_1, numero_2))
         print(numero_1 - numero_2)
 
     # Multiplicação
-    elif operação == '*':
+    elif operacao == '*':
         print('{} * {} = '.format(numero_1, numero_2))
         print(numero_1 * numero_2)
 
     # Divisão
-    elif operação == '/':
+    elif operacao == '/':
         print('{} / {} = '.format(numero_1, numero_2))
         print(numero_1 / numero_2)
 
     else:
         print('Você não digitou um operador válido, execute o programa novamente.')
 
-# Chamamos calculate() fora da função
-calculate()
+    # Após o cálculo, pergunte se o usuário quer continuar
+    again()
 
+# Define função again() para perguntar se o usuario quer usar a calculadora de novo
+def again():
+    # Recebe input do usuario
+    calc_again = input('''
+Você quer calcular de novo?
+Por favor digite S para SIM ou N para NÃO.
+''')
+
+    if calc_again.upper() == 'S':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('Até mais!')
+    else:
+        again()
+
+# chama calculate() fora da função
+calculate()
